5a1e769784b3b6e3546bba3519b159a1276f6a8e
Michelle Lim <michelle.lim@yale.edu>
2017-09-01 17:56:43 -0400

    successfully displayed current location

--- GoTo/AppleMapsViewController.swift
+++ GoTo/AppleMapsViewController.swift
@@ -12,7 +12,7 @@ import SVProgressHUD
 import Mapbox
 
 // View controller for Apple Maps Example
-class AppleMapsViewController: UIViewController, IALocationManagerDelegate {
+class AppleMapsViewController: UIViewController, MGLMapViewDelegate, IALocationManagerDelegate {
     
     var mapView: MGLMapView!
     var label = UILabel()
@@ -29,6 +29,7 @@ class AppleMapsViewController: UIViewController, IALocationManagerDelegate {
         // Set the map’s center coordinate and zoom level.
         mapView.setCenter(CLLocationCoordinate2D(latitude: 41.31569, longitude: -72.92562), zoomLevel: 19, animated: false)
         view.addSubview(mapView)
+                mapView.delegate = self
         
         // Show spinner while waiting for location information from IALocationManager
         SVProgressHUD.show(withStatus: NSLocalizedString("Waiting for location data", comment: ""))
@@ -150,5 +151,42 @@ class AppleMapsViewController: UIViewController, IALocationManagerDelegate {
         
         
         SVProgressHUD.dismiss()
+    }
+    
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        // Try to reuse the existing ‘pisa’ annotation image, if it exists.
+        var annotationImage = mapView.dequeueReusableAnnotationImage(withIdentifier: "pisa")
+        
+        if annotationImage == nil {
+            var image = UIImage(named: "circle")!
+            
+            // The anchor point of an annotation is currently always the center. To
+            // shift the anchor point to the bottom of the annotation, the image
+            // asset includes transparent bottom padding equal to the original image
+            // height.
+            //
+            // To make this padding non-interactive, we create another image object
+            // with a custom alignment rect that excludes the padding.
+            image = image.withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 0, bottom: image.size.height/2, right: 0))
+            image = ResizeImage(image: image, targetSize: CGSize(width: 20, height: 20.0))
+            // Initialize the ‘pisa’ annotation image with the UIImage we just loaded.
+            annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "pisa")
+        }
+        
+        return annotationImage
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always allow callouts to popup when annotations are tapped.
+        return true
+    }
+    
+    //from https://stackoverflow.com/questions/2658738/the-simplest-way-to-resize-an-uiimage
+    func ResizeImage(image: UIImage, targetSize: CGSize) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(targetSize, false, 0.0);
+        image.draw(in: CGRect(origin: CGPoint.zero, size: CGSize(width: targetSize.width, height: targetSize.height)))
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
     }
 }
