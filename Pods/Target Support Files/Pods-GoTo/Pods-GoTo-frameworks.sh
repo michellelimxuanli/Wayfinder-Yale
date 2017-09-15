5aecf49f435211801a202a97a933fad2f055c7f8
Michelle Lim <michelle.lim@yale.edu>
2017-09-14 23:46:25 -0400

    added activity indicator

--- Pods/Target Support Files/Pods-GoTo/Pods-GoTo-frameworks.sh
+++ Pods/Target Support Files/Pods-GoTo/Pods-GoTo-frameworks.sh
@@ -103,6 +103,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GradientCircularProgress/GradientCircularProgress.framework"
   install_framework "${PODS_ROOT}/IndoorAtlas/indooratlas-ios-sdk/IndoorAtlas.framework"
   install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
   install_dsym "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework.dSYM"
@@ -110,6 +111,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GradientCircularProgress/GradientCircularProgress.framework"
   install_framework "${PODS_ROOT}/IndoorAtlas/indooratlas-ios-sdk/IndoorAtlas.framework"
   install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
   install_dsym "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework.dSYM"
